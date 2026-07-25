8fa4fc4cd95204ee4ed74239a7661152381e5465
r25347sh <r25347sh@hs.reitaku.jp>
2026-07-26 07:22:04 +0900

    Create load.js

---
+++ js/load.js
@@ -0,0 +1,31 @@
+(function() {
+  // 読み込みたい外部ファイルのリスト
+  const resources = {
+    css: [
+      '/reitansai/MEMO/MEMO.css',
+      '/reitansai/gesture/pen.css'
+    ],
+    js: [
+      '/reitansai/MENU/MENU.js',
+      '/reitansai/gesture/pen.js',
+      '/reitansai/gesture/action.js',
+      '/reitansai/gesture/gesture.js'
+      ]
+  };
+
+  // CSSの自動読み込み
+  resources.css.forEach(url => {
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = url;
+    document.head.appendChild(link);
+  });
+
+  // JavaScriptの自動読み込み
+  resources.js.forEach(url => {
+    const script = document.createElement('script');
+    script.src = url;
+    script.defer = true; // 読み込み順序や実行タイミングの制御
+    document.body.appendChild(script);
+  });
+})();
