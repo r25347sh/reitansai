aead67f776a023d8cbfd2a1f0e566099bfb8a2a2
r25347sh <r25347sh@hs.reitaku.jp>
2026-07-31 19:06:02 +0900

    Update MENU.js

--- MENU/MENU.js
+++ MENU/MENU.js
@@ -11,7 +11,7 @@ const RADIAL_MENU_DATA = [
       { label: 'データサイエンス探究 AIゼミ', icon: '📊', url: '/reitansai/pages/zemi/data-science-ai.html' },
       { label: '教育ゼミ', icon: '📖', url: '/reitansai/pages/zemi/kyoiku.html' },
       { label: '国際地域研究ゼミ', icon: '🌍', url: '/reitansai/pages/zemi/kokusai-chiiki.html' },
-      { label: '文藝創作ゼミ', icon: '🖋️', url: '/reitansai/pages/zemi/bungei-sakuhin.html' },
+      { label: '文芸小説創作ゼミ', icon: '🖋️', url: '/reitansai/pages/zemi/bungei-shosetsu-sosaku.html' },
       { label: '化学ゼミ', icon: '🧪', url: '/reitansai/pages/zemi/kagaku.html' },
       { label: '文学ゼミ', icon: '📜', url: '/reitansai/pages/zemi/bungaku.html' },
       { label: 'メディアゼミ', icon: '📺', url: '/reitansai/pages/zemi/media.html' },
@@ -20,9 +20,14 @@ const RADIAL_MENU_DATA = [
       { label: '観光ゼミ', icon: '🗼', url: '/reitansai/pages/zemi/kanko.html' },
       { label: '語学ゼミ', icon: '🗣️', url: '/reitansai/pages/zemi/gengo.html' },
       { label: '遊びの探究ゼミ', icon: '🎮', url: '/reitansai/pages/zemi/asobi-tankyu.html' },
-      { label: '医療ゼミ', icon: '🩺', url: '/reitansai/pages/zemi/iryo.html' },
-      { label: '映像クリエイターゼミ', icon: '🎥', url: '/reitansai/pages/zemi/eizo-creator.html' },
-      { label: 'スポーツゼミ', icon: '⚽', url: '/reitansai/pages/zemi/sports.html' }
+      {
+        label:'外部企業ゼミ',
+        icon:'',
+        items: [
+          { label: '映像編集ゼミ', icon: '⏯️', url: '/reitansai/pages/zemi/eizo-henshu.html' },
+          { label: 'デジタルコンテンツ制作ゼミ', icon: '01', url: '/reitansai/pages/zemi/digital-content-create.html' },
+          { label: 'イベント企画ゼミ', icon: '🎪', url: '/reitansai/pages/zemi/event-kikaku.html' }
+        ]
     ]
   },
   { label: '麗探祭', icon: '🎉', url: '/reitansai/pages/event.html' },
