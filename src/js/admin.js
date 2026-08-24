dedd13815d2f4f389b674ab61af20810c91431bb
r25347sh <r25347sh@hs.reitaku.jp>
2026-08-25 06:43:35 +0900

    Update admin.js

--- src/js/admin.js
+++ src/js/admin.js
@@ -1,8 +1,8 @@
 // ==========================================
 // ⚙️ GitHub設定（★ご自身のアカウント・リポジトリ名に書き換えてください！）
 // ==========================================
-const GITHUB_OWNER = "あなたのGitHubユーザー名"; 
-const GITHUB_REPO = "あなたのリポジトリ名";     
+const GITHUB_OWNER = "r25347sh"; 
+const GITHUB_REPO = "reitansai";     
 const GITHUB_TOKEN = "github_pat_11BXRNCFA0EVBbGiXBnXgp_"+"rHoCChQCXXzyvyk2ox1l9RMI3xtQRwRmqUHVNAiAEsjWFWDH6TVCdEu2Pjo";
 
 // ==========================================
